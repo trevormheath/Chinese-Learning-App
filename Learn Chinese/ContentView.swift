8d911e2d97ae24e43dc203fb14aa7359e8a7ba14
trevormheath <trevormheath@gmail.com>
2024-10-17 08:14:49 -0600

    Initial Commit

---
+++ Learn Chinese/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Learn Chinese
+//
+//  Created by IS 543 on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Learn Chinese/Learn_ChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_ChineseApp.swift
+//  Learn Chinese
+//
+//  Created by IS 543 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Learn_ChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
